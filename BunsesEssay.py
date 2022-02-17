130888e135777b1abd641bcda6888a835d39d553
ClassicMC <classicmclive@gmail.com>
2022-02-17 12:48:46 -0500

    Add files via upload
    An little story I made using bunsesScript 0.0.1 for python

---
+++ BunsesEssay.py
@@ -0,0 +1,84 @@
+"""
+Bunses, a Java/Python based language,
+developed by, ClassicMC.
+credits(); for more info.
+"""
+def printf(bunses):
+    print(bunses)
+def loop(text, x, start,end):
+    while x:
+        start +=1;
+        print(text);
+        if start == end:
+            x = False;
+def credits():
+    printf("Made by, ClassicMC")
+    printf("v0.0.1 pythonEdition")
+    printf("helpf() for more.")
+    printf("Semicolons are not required,")
+    printf("Bunses is based on \\\"prototype e-sharp\\\" for game delopment.")
+def helpf():
+    print("to print items printf(\"//print stuff\")");
+    print("to loop loop(\"//loop text\",true,//startingAmount,//endingAmount\")");
+    print("\\n for a new line, \\\", and \\\', \\\\ for avoding those symbols in strings");
+    print("If you are experincing errors,double check that your code has no semicolons \";\"");
+#Main code loop    
+printf("Printf is the most useless thing in the world.");
+printf("When you could just use print.");
+printf("But hey, its java compatable.");
+printf("Only if you use Bunses for java.");
+printf("But you'd still have to add semicolons.");
+printf("The loop function is cool though.");
+printf("Its very useful in python or java.");
+printf("Maybe I should make a C# port.");
+printf("It would be very nice.");
+printf("I don't know why I'm still using the \"printf\" format anyway.");
+printf("I should probably stop.");
+printf("But hey, your still here.");
+printf("Maybe you like this still.");
+printf("And you should probably still use the semicolons.");
+printf("For backwards compatability with the java version.");
+printf("But thats just your choice isn't it.");
+printf("It just all depends on wether you like java.");
+printf("Doesn't it.");
+printf("But hey,");
+printf("I like java.");
+printf("And thats all that matters right?");
+printf("I'm making this just like older days.");
+printf("Where all you could do was print.");
+printf("Imagine making essays using print.");
+printf("Over and over and over again.");
+printf("I don't know why I'm doing this...");
+printf("But its kinda fun.");
+printf("This is just a whole bunch of text.");
+printf("Many useless lines of text");
+printf("But I like it.");
+printf("...");
+printf("And I would hope you liked it.");
+printf("But I don't control your opinon.");
+printf("Thats up to you.");
+printf("It's in the name.");
+printf("Your opinion.");
+printf("...");
+printf("...");
+printf("Why was this language even made.");
+printf("It isn't nessacary.");
+printf("You could do all of this in vanilla java or python.");
+printf("So why.");
+printf("It's all just an illusion.");
+printf("A trick.");
+printf("To think that you are using something that would help.");
+printf("When you really aren't.");
+printf("And you think that this is all worth it in the end when it's not.");
+printf("It's devestating.");
+printf("...");
+printf("But the whole world won't suffer because of this.");
+printf("Some may...");
+printf("But most wont");
+printf("And to think that most won't suffer.");
+printf("It's comforting.");
+printf("At least to me.");
+printf("But go ahead.");
+printf("Write an entire essay in bunses.");
+printf("It wont hurt you.");
+printf("It might all be woth it in the end...");
